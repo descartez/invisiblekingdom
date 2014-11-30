6b954750782616efd2f8a032d9fe02e26092c0af
Ryan Stong <rvanstong@gmail.com>
2014-11-30 14:41:03 -0800

    finished database and seeding

--- db/migrate/20141129171420_create_stories.rb
+++ db/migrate/20141129171420_create_stories.rb
@@ -1,6 +1,7 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
+      t.belongs_to :user
       t.string :content
       t.integer :longitude
       t.integer :lattitude
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 10.times do
-  User.new(username: Faker::Internet.user_name, email: Faker::Internet.email, password: "password")
+  new_user = User.create!(username: Faker::Internet.user_name, email: Faker::Internet.email, password: "password")
+  15.times do
+    stories = ["The Emperor decreed, let there be a mighty gate, gold and wide.", "Shades and echoes of shades.", "Keep an eye out for fairies at this time of day. Just look with the corner of your eyes.", "Breathe.", "When you ask to enter, make sure to say 'please'", "Make sure to listen to Old Trees.", "You stand on many hills."]
+    new_story = Story.create!
+    new_story.story_maker
+    new_user.stories << new_story
+  end
 end
 
-15.times do
-  stories = ["The Emperor decreed, let there be a mighty gate, gold and wide.", "Shades and echoes of shades.", "Keep an eye out for fairies at this time of day. Just look with the corner of your eyes.", "Breathe.", "When you ask to enter, make sure to say 'please'", "Make sure to listen to Old Trees.", "You stand on many hills."]
-  Story.new(content: stories.shuffle.sample, user_id: rand(1..10))
-end
